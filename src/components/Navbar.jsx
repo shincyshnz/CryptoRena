54c8b9b8cca9c9b5e43d15c0b848245a540c4caa
Shincy <shincyshnz@gmail.com>
2023-09-24 10:53:45 +0530

    Menu mobile view edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Typography, Avatar } from "antd";
 import icon from "../images/crypto.png";
 import { Link } from "react-router-dom";
 import CustomMenu from "./CustomMenu";
+import { MenuOutlined } from "@ant-design/icons";
 
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize <= 800) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
   return (
     <div className="nav-container">
       <div className="logo-container">
@@ -12,9 +34,21 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
           <Link to="/">CryptoRena</Link>
         </Typography.Title>
+        <button
+          className="menu-control-container"
+          style={{ backgroundColor : "transparent"}}
+          onClick={() => setActiveMenu(!activeMenu)}
+        >
+          <MenuOutlined
+            style={{
+              fontSize: "48px",
+              color: "white",
+              backgroundColor: "#001529",
+            }}
+          />
+        </button>
       </div>
-
-      <CustomMenu />
+      {activeMenu && <CustomMenu />}
     </div>
   );
 };
